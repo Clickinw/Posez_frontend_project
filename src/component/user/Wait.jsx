591ef38ea27fc15a9f212ae72295d407a9402d79
Chanakarn <chanakarn_h@cmu.ac.th>
2022-11-16 00:32:57 +0700

    wait_order

---
+++ src/component/user/Wait.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Wait() {
+  return (
+    <fragment>
+      <div>
+        <div className="mb-4 flex justify-center content-center align-center">
+          <img className="w-[30%]" src="src\assets\PosezFinal.png" alt="" />
+        </div>
+        <div className="container">
+          <div>
+            <h3 className="text-4xl mb-5 text-[#3C4048] ">สถานะคำสั่งซื้อ</h3>
+          </div>
+          <div className="grid lg:grid-cols-1 md:grid-cols-1 grid-cols-1">
+            <div className="flex justify-center content-center align-center">
+              <div className="rounded-lg bg-[#FCFBF1] w-80 h-full shadow-xl shadow-gray-300">
+                <div className="flex justify-center content-center align-center m-3">
+                  <img
+                    className="w-[100%] "
+                    src="https://cdn-icons-png.flaticon.com/512/1027/1027230.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </fragment>
+  );
+}
