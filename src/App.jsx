fbb659013cc2390f04cb567b61337c35173b3ff5
Clickinw <inwclick17694@outlook.com>
2022-11-17 00:26:34 +0700

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import "./App.css";
-import Logo from "./component/Logo";
-import HomeUser from "./component/user/HomeUser";
-import Menu from "./component/user/Menu";
-import List from "./component/user/List";
+import React from 'react';
+import './App.css';
+import Logo from './component/Logo';
+import HomeUser from './component/user/HomeUser';
+import Menu from './component/user/Menu';
+import List from './component/user/List';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      {/* <HomeUser /> */}
-      {/* <Menu /> */}
-      <List />
+      <Router>
+        <Logo />
+        <Route exact path='/'>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home' component={HomeUser} />
+        <Route path='/menu' component={Menu} />
+        <Route path='/list' component={List} />
+      </Router>
     </>
   );
 }
