619d6a88364dec1b30b88ea921a4941ad0722329
Chanakarn <chanakarn_h@cmu.ac.th>
2022-11-18 01:19:44 +0700

    List

--- src/component/user/List.jsx
+++ src/component/user/List.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import data from "../../api/data";
 
 export default function List() {
   return (
@@ -9,75 +10,45 @@ export default function List() {
         </div>
         <div className="container">
           <div>
-            <h3 className="text-4xl mb-5 text-[#3C4048] ">รายการอาหาร</h3>
+            <h3 className="text-4xl text-[#3C4048] ">รายการอาหาร</h3>
           </div>
           <div className="grid lg:grid-cols-1 md:grid-cols-1 grid-cols-1">
             <div className="rounded-lg bg-[#FCFBF1] w-full h-full shadow-xl shadow-gray-300">
-              <div className="px-10 py-5 flex justify-center content-center align-center text-[#3C4048] text-sm">
-                <table class="table-fixed">
-                  <thead>
-                    <tr>
-                      <th className="px-4 pb-4">รายการ</th>
-                      <th className="px-4 pb-4">จำนวน</th>
-                      <th className="px-4 pb-4">ตัวเลือก</th>
-                      <th className="px-4 pb-4">ราคา</th>
-                      <th className="px-4 pb-4">หมายเหตุ</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td className="px-4 pb-2">ต้มยำกุ้ง</td>
-                      <td className="px-4 pb-2">2</td>
-                      <td className="px-4 pb-2"></td>
-                      <td className="px-4 pb-2">178</td>
-                      <td className="px-4 pb-2">เผ็ดๆ</td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 pb-2">ปีกไก่ทอดน้ำปลา</td>
-                      <td className="px-4 pb-2">1</td>
-                      <td className="px-4 pb-2"></td>
-                      <td className="px-4 pb-2">49</td>
-                      <td className="px-4 pb-2"></td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 pb-2">กะเพราหมูสับ</td>
-                      <td className="px-4 pb-2">2</td>
-                      <td className="px-4 pb-2">ไข่ดาว</td>
-                      <td className="px-4 pb-2">78</td>
-                      <td className="px-4 pb-2">เผ็ดๆ</td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 pb-2">Service Charge</td>
-                      <td className="px-4 pb-2"></td>
-                      <td className="px-4 pb-2"></td>
-                      <td className="px-4 pb-2">7</td>
-                      <td className="px-4 pb-2"></td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 pt-3"></td>
-                      <td className="px-4 pt-3"></td>
-                      <td className="px-4 pt-3">รวม</td>
-                      <td className="px-4 pt-3">312</td>
-                      <td className="px-4 pt-3"></td>
-                    </tr>
-                  </tbody>
-                </table>
+              <div className="px-10 py-3 text-xl text-[#3C4048]">
+                <div className="grid grid-cols-5 gap-4">
+                  <div>รายการ</div>
+                  <div>จำนวน</div>
+                  <div>ตัวเลือก</div>
+                  <div>ราคา</div>
+                  <div>หมายเหตุ</div>
+                </div>
               </div>
+              {data.map((data) => (
+                <div className="px-10 text-[#3C4048]">
+                  <div className="grid grid-cols-5 gap-4 mb-2 justify-center content-center align-center">
+                    <div>{data.title}</div>
+                    <div>-</div>
+                    <div></div>
+                    <div>{data.price}</div>
+                    <div>-</div>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="mt-3">
-              <button
-                type="submit"
-                className="m-3 bg-[#ED7072] hover:bg-[#EBA78A] rounded-md shadow-xl shadow-gray-300 mb-3 w-[20%] h-[2rem]"
-              >
-                ย้อนกลับ
-              </button>
-              <button
-                type="submit"
-                className="m-3 bg-[#F5B449] hover:bg-[#CE3434] rounded-md shadow-xl shadow-gray-300 mb-3 w-[20%] h-[2rem]"
-              >
-                ยืนยันออเดอร์
-              </button>
-            </div>
+          </div>
+          <div className="mt-3">
+            <button
+              type="submit"
+              className="m-3 bg-[#ED7072] hover:bg-[#EBA78A] rounded-md shadow-xl shadow-gray-300 mb-3 w-[20%] h-[2rem]"
+            >
+              ย้อนกลับ
+            </button>
+            <button
+              type="submit"
+              className="m-3 bg-[#F5B449] hover:bg-[#CE3434] rounded-md shadow-xl shadow-gray-300 mb-3 w-[20%] h-[2rem]"
+            >
+              ยืนยันออเดอร์
+            </button>
           </div>
         </div>
       </div>
