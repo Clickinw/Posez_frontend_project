747276f68d85112ad65daf79d3993f788eed33a1
Chanakarn <chanakarn_h@cmu.ac.th>
2022-11-17 22:58:59 +0700

    table

---
+++ src/component/user/Table.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+
+export default function Table() {
+  return (
+    <fragment>
+      <div className="mb-4 flex justify-center content-center align-center">
+        <img className="w-[40%]" src="src\assets\PosezFinal.png" alt="" />
+      </div>
+      <div className="container  px-8">
+        <div>
+          <h3 className="text-4xl mb-5 text-[#3C4048]">กรุณาเลือกโต๊ะ</h3>
+        </div>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+          <div className="">
+            <button className="w-[100%] h-[6rem] bg-[#CE3434] hover:bg-[#8D201A]  rounded-xl shadow-xl shadow-gray-300 ">
+              โต๊ะ 1
+              <div className="flex justify-center content-center align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-user"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#000000"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="7" r="4" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />4
+                </svg>
+                <p>4</p>
+              </div>
+            </button>
+          </div>
+          <div className="">
+            <button className="w-[100%] h-[6rem] bg-[#CE3434] hover:bg-[#8D201A]  rounded-xl shadow-xl shadow-gray-300 ">
+              โต๊ะ 2
+              <div className="flex justify-center content-center align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-user"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#000000"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="7" r="4" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />4
+                </svg>
+                <p>4</p>
+              </div>
+            </button>
+          </div>
+          <div className="">
+            <button className="w-[100%] h-[6rem] bg-[#CE3434] hover:bg-[#8D201A]  rounded-xl shadow-xl shadow-gray-300 ">
+              โต๊ะ 3
+              <div className="flex justify-center content-center align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-user"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#000000"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="7" r="4" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />4
+                </svg>
+                <p>4</p>
+              </div>
+            </button>
+          </div>
+          <div className="">
+            <button className="w-[100%] h-[6rem] bg-[#CE3434] hover:bg-[#8D201A]  rounded-xl shadow-xl shadow-gray-300 ">
+              โต๊ะ 4
+              <div className="flex justify-center content-center align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-user"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#000000"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="7" r="4" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />4
+                </svg>
+                <p>4</p>
+              </div>
+            </button>
+          </div>
+          <div className="">
+            <button className="w-[100%] h-[6rem] bg-[#CE3434] hover:bg-[#8D201A]  rounded-xl shadow-xl shadow-gray-300 ">
+              โต๊ะ 5
+              <div className="flex justify-center content-center align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-user"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#000000"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="7" r="4" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />4
+                </svg>
+                <p>4</p>
+              </div>
+            </button>
+          </div>
+          <div className="">
+            <button className="w-[100%] h-[6rem] bg-[#CE3434] hover:bg-[#8D201A]  rounded-xl shadow-xl shadow-gray-300 ">
+              โต๊ะ 6
+              <div className="flex justify-center content-center align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-user"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#000000"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <circle cx="12" cy="7" r="4" />
+                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />4
+                </svg>
+                <p>4</p>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </fragment>
+  );
+}
