3645e3942c9e2a3b9bd580b691c805c4acd0eb70
Clickinw <inwclick17694@outlook.com>
2022-11-17 16:58:45 +0700

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,16 @@ import Logo from './component/Logo';
 import HomeUser from './component/user/HomeUser';
 import Menu from './component/user/Menu';
 import List from './component/user/List';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Router>
-        <Logo />
-        <Route exact path='/'>
-          <Redirect to='/home' />
-        </Route>
-        <Route path='/home' component={HomeUser} />
-        <Route path='/menu' component={Menu} />
-        <Route path='/list' component={List} />
-      </Router>
+      <Routes>
+        <Route path='/home' element={<HomeUser />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/list' element={<List />} />
+      </Routes>
     </>
   );
 }
