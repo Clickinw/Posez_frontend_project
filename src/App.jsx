dbbafdc24740c1e9d26fe513deab885535b5f029
Chanakarn <chanakarn_h@cmu.ac.th>
2022-11-17 18:02:57 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import Logo from "./component/Logo";
 import HomeUser from "./component/user/HomeUser";
 import Menu from "./component/user/Menu";
 import List from "./component/user/List";
+import Wait from "./component/user/Wait";
+import Payment from "./component/user/Payment";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <HomeUser /> */}
-      {/* <Menu /> */}
-      <List />
+      <Routes>
+        <Route path="/home" element={<HomeUser />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/Wait" element={<Wait />} />
+        <Route path="/Payment" element={<Payment />} />
+      </Routes>
     </>
   );
 }
