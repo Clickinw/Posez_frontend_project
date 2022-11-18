919e6eae92d330ef0f1aa36fd7636f2d3b312522
Clickinw <inwclick17694@outlook.com>
2022-11-18 12:15:08 +0700

    update 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import "./App.css";
-import Logo from "./component/Logo";
-import HomeUser from "./component/user/HomeUser";
-import Menu from "./component/user/Menu";
-import List from "./component/user/List";
-import Wait from "./component/user/Wait";
-import Payment from "./component/user/Payment";
-import NotFound from "./component/user/NotFound";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import Logo from './component/Logo';
+import HomeUser from './component/user/HomeUser';
+import Menu from './component/user/Menu';
+import List from './component/user/List';
+import Wait from './component/user/Wait';
+import Payment from './component/user/Payment';
+import NotFound from './component/user/NotFound';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="
-        <Route path="/home" element={<HomeUser />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/list" element={<List />} />
-        <Route path="/Wait" element={<Wait />} />
-        <Route path="/Payment" element={<Payment />} />
+        <Route path='/' element={<HomeUser />} />
+        <Route path='/home' element={<HomeUser />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/list' element={<List />} />
+        <Route path='/Wait' element={<Wait />} />
+        <Route path='/Payment' element={<Payment />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
