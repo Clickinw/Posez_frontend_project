fea2e6f1ff08e2aea36e2e97f4effc51b0172422
Clickinw <inwclick17694@outlook.com>
2022-11-18 12:12:29 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Logo from './component/Logo';
 import HomeUser from './component/user/HomeUser';
 import Menu from './component/user/Menu';
+import NotFound from './component/NotFound';
 import List from './component/user/List';
 import { Routes, Route } from 'react-router-dom';
 
@@ -10,9 +11,11 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path='/' element={<HomeUser />} />
         <Route path='/home' element={<HomeUser />} />
         <Route path='/menu' element={<Menu />} />
         <Route path='/list' element={<List />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
