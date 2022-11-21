863db97f79818f74907a075fadbe5ca1f273f08e
Chanakarn <chanakarn_h@cmu.ac.th>
2022-11-22 04:22:50 +0700

    delete-finish

--- src/component/seller/Delete.jsx
+++ src/component/seller/Delete.jsx
@@ -11,24 +11,24 @@ export default function Delete() {
         </div> */}
         <div className="grid lg:grid-cols-1 md:grid-cols-1 grid-cols-1">
           <div>
-            <h3 className="text-3xl mb-5 mt-5 text-[#3C4048]">รายการอาหาร</h3>
+            <h3 className="text-4xl mb-5 mt-5 text-[#3C4048]">รายการอาหาร</h3>
             <div className="grid grid-rows">
               <div className="rounded-lg bg-[#FCFBF1] w-full h-full shadow-xl shadow-gray-300  text-[#3C4048]">
                 <div className="px-5 py-3 text-xl">
                   <div className="grid grid-cols-4 gap-4 border-b-2">
-                    <div>ID</div>
-                    <div>ชื่อ</div>
-                    <div>ราคา</div>
-                    <div>เพิ่มเติม</div>
+                    <span className="text-start">ID</span>
+                    <span className="text-start">ชื่อ</span>
+                    <span className="text-center">ราคา</span>
+                    <span className="text-center">เพิ่มเติม</span>
                   </div>
                 </div>
                 {data.map((data) => (
                   <div className="px-5 text-[#3C4048]">
-                    <div className="grid grid-cols-4 gap-4 mb-2 justify-center content-center align-center border-b">
-                      <div>{data.id}</div>
-                      <div>{data.title}</div>
-                      <div>{data.price}</div>
-                      <div>
+                    <div className="grid grid-cols-4 gap-4 mb-2 border-b">
+                      <span className="text-start">{data.id}</span>
+                      <span className="text-start">{data.title}</span>
+                      <span className="text-center">{data.price}</span>
+                      <span className="text-center">
                         <button
                           type="submit"
                           className=" bg-[#F5B449] hover:bg-gray-200  rounded-md shadow-xl shadow-gray-300 w-12 h-[1.5rem] mb-2 mx-2"
@@ -41,7 +41,7 @@ export default function Delete() {
                         >
                           ลบ
                         </button>
-                      </div>
+                      </span>
                     </div>
                   </div>
                 ))}
