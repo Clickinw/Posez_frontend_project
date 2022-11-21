e9c81dc53a9b5b9640f83db6ef57820324126ddc
Clickinw <inwclick17694@outlook.com>
2022-11-21 17:25:58 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Router } from 'react-router-dom';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
 
