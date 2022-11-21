e164a0c18adb2cb3893cb30618faaa34e203bb43
Clickinw <inwclick17694@outlook.com>
2022-11-21 13:34:07 +0700

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import React from "react";
-import "./App.css";
-import "tw-elements";
-// import Logo from "./component/Logo";
-import HomeUser from "./component/user/HomeUser";
-// import Menu from "./component/user/Menu";
-// import List from "./component/user/List";
-// import Wait from "./component/user/Wait";
-// import Payment from "./component/user/Payment";
-import Modal from "./component/user/Modal";
-import Takeaway from "./component/user/Takeaway";
-import Res from "./component/user/Res";
-import Navbar from "./component/seller/Navbar";
-import Order from "./component/seller/Order";
-import Select1 from "./component/Select1";
-// import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import 'tw-elements';
+import Logo from './component/Logo';
+import HomeUser from './component/user/HomeUser';
+import Menu from './component/user/Menu';
+import List from './component/user/List';
+import Wait from './component/user/Wait';
+import Payment from './component/user/Payment';
+import Modal from './component/user/Modal';
+import Takeaway from './component/user/Takeaway';
+import Res from './component/user/Res';
+import Navbar from './component/seller/Navbar';
+import Order from './component/seller/Order';
+import Select1 from './component/Select1';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      {/* <Routes>
-        <Route path="/home" element={<HomeUser />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/list" element={<List />} />
-        <Route path="/Wait" element={<Wait />} />
-        <Route path="/Payment" element={<Payment />} />
-        <Route path="/Modal" element={<Modal />} />
-      </Routes> */}
-      {/* <Modal /> */}
-      {/* <Takeaway /> */}
-      {/* <Res /> */}
-      {/* <Navbar /> */}
+      <Routes>
+        <Route path='/home' element={<HomeUser />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/list' element={<List />} />
+        <Route path='/Wait' element={<Wait />} />
+        <Route path='/Payment' element={<Payment />} />
+        <Route path='/Modal' element={<Modal />} />
+        <Route path='/Takeaway' element={<Takeaway />} />
+        <Route path='/Res' element={<Res />} />
+        <Route path='/Navbar' element={<Navbar />} />
+        <Route path='/Order' element={<Order />} />
+        <Route path='/Select1' element={<Select1 />} />
+      </Routes>
       <Order />
-      {/* <Select1 /> */}
     </>
   );
 }
