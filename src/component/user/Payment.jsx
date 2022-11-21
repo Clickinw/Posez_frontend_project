158100fc35bbc4ea925c23b23122ef5acce7b4ee
Chanakarn <chanakarn_h@cmu.ac.th>
2022-11-22 03:17:46 +0700

    Payment-finish

--- src/component/user/Payment.jsx
+++ src/component/user/Payment.jsx
@@ -3,59 +3,65 @@ import React from "react";
 export default function Payment() {
   return (
     <fragment>
-      <div className="mb-3 flex justify-center content-center align-center">
-        <img className="w-[30%]" src="src\assets\PosezFinal.png" alt="" />
-      </div>
-      <div className="container">
-        <div>
-          <h3 className="text-4xl mb-3 text-[#3C4048]">ชำระเงิน</h3>
+      <div>
+        <div className="mb-4 mt-12 flex justify-center content-center align-center">
+          <img className="w-1/5" src="src\assets\PosezFinal.png" alt="" />
         </div>
-        <div className="grid lg:grid-cols-1 md:grid-cols-1 grid-cols-1">
-          <div className="flex justify-center content-center align-center">
-            <div className="rounded-lg bg-[#FCFBF1] w-[80%] h-full shadow-xl shadow-gray-300">
-              <div className="inline-flex gap-6 mt-4 ">
-                <h3 className="text-2xl text-[#3C4048]">รวม: </h3>
-                <h3 className="text-2xl text-[#3C4048]">312</h3>
-              </div>
-              <div className="border-b-2 border-gray-300 mx-5"></div>
-              <div className="flex justify-center content-center align-center gap-5 m-5 ">
-                <div className="inline-flex gap-6 justify-center content-center align-center">
-                  <div className="grid grig-row-2">
-                    <h3 className="text-2xl text-[#3C4048]">เงินสด</h3>
-                    <img
-                      className="w-[15rem] h-[15rem]shadow-xl shadow-gray-300"
-                      src="https://cdn-icons-png.flaticon.com/512/2543/2543190.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className="border-l-2 border-gray-500"></div>
-                  <div className="grid grig-row-2">
-                    <h3 className="text-2xl mb-3 text-[#3C4048]">สแกนจ่าย</h3>
-                    <img
-                      className="w-[15rem] h-[10rem] shadow-xl shadow-gray-300"
-                      src="https://sv1.img.in.th/SK8dq.jpg"
-                      alt=""
-                    />
+        <div className="grid grid-rows justify-center content-center align-center">
+          <h3 className="text-5xl mb-5 text-[#3C4048] text-center">
+            การชำระเงิน
+          </h3>
+          <div className="grid lg:grid-cols-1 md:grid-cols-1 grid-cols-1">
+            <div className="rounded-lg bg-[#FCFBF1] w-full h-full shadow-xl shadow-gray-300">
+              <div className="m-3 px-10 py-5">
+                <div className="grid grid-cols-2 justify-center content-center align-center mx-3 text-xl text-[#3C4048] border-b-2  border-gray-400">
+                  <sapn className="text-end underline underline-offset-2 m-2">
+                    รวม:{" "}
+                  </sapn>
+                  <span className="text-start m-2"> xxx บาท</span>
+                </div>
+                <div className="flex justify-center content-center align-center mt-3 mb-4">
+                  <div className="inline-flex gap-6 justify-center content-center align-center">
+                    <div className="grid grig-row-2">
+                      <span className="text-2xl text-[#3C4048] text-center">
+                        เงินสด
+                      </span>
+                      <img
+                        className="w-[15rem] h-[15rem]shadow-xl shadow-gray-300"
+                        src="https://cdn-icons-png.flaticon.com/512/2543/2543190.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className="border-l-2 border-gray-400"></div>
+                    <div className="grid grig-row-2">
+                      <span className="text-2xl mb-3 text-[#3C4048] text-center">
+                        สแกนจ่าย
+                      </span>
+                      <img
+                        className="w-[15rem] h-[15rem] shadow-lg shadow-gray-300"
+                        src="https://sv1.img.in.th/SK8dq.jpg"
+                        alt=""
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="border-t-2 border-gray-300 mx-5 mb-3"></div>
-              <div className="flex justify-center content-center align-center">
-                <div class="mb-3">
-                  <label for="formFileSm" class="form-label  text-[#3C4048]">
-                    โปรดแนบหลักฐานการโอนเงิน
-                  </label>
-                  <div class="flex items-center  py-2">
-                    <input
-                      className="form-control appearance-none bg-white bg-clip-padding border-none w-full text-[#3C4048] mr-3 py-1 px-2 leading-tight focus:outline-none shadow-xl shadow-gray-300"
-                      type="file"
-                    />
-                    <button
-                      class="flex-shrink-0 bg-[#F5B449] hover:bg-[#CE3434] rounded-md shadow-xl shadow-gray-300 py-1 px-2 w-[20%] "
-                      type="button"
-                    >
-                      ส่ง
-                    </button>
+                <div className="flex justify-center content-center align-center border-t-2  border-gray-400">
+                  <div class="mb-3 mt-3">
+                    <label for="formFileSm" class="form-label  text-[#3C4048]">
+                      โปรดแนบหลักฐานการโอนเงิน
+                    </label>
+                    <div class="flex items-center  py-2">
+                      <input
+                        className="form-control appearance-none rounded-md border-2 bg-[#CE3434] bg-clip-padding border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none shadow-xl shadow-gray-300"
+                        type="file"
+                      />
+                      <button
+                        class="flex-shrink-0 bg-[#F5B449] hover:bg-[#CE3434] rounded-md shadow-xl shadow-gray-300 py-1 px-2 w-[20%] "
+                        type="button"
+                      >
+                        ส่ง
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
