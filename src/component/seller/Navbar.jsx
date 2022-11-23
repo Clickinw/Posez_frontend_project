ca02592f3ea5672c7fb495141bc267d8b72cf949
Clickinw <inwclick17694@outlook.com>
2022-11-23 12:13:41 +0700

    fix nav

--- src/component/seller/Navbar.jsx
+++ src/component/seller/Navbar.jsx
@@ -1,58 +1,44 @@
-import { React, useState } from "react";
-import { BsPersonCircle } from "react-icons/bs";
-import { RiDashboardFill } from "react-icons/ri";
-import { GiCampCookingPot } from "react-icons/Gi";
-import { BiTimer } from "react-icons/bi";
-import { CgPlayListAdd } from "react-icons/Cg";
-import { AiOutlineDelete } from "react-icons/Ai";
-import { MdOutlineLogout } from "react-icons/Md";
-import { Link } from "react-router-dom";
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   const [open, setOpen] = useState(true);
   const Menus = [
     {
-      title: "ออเดอร์",
-      icon: <GiCampCookingPot />,
-      Link: "/order",
+      title: 'ออเดอร์',
+
+      Link: '/order',
     },
     {
-      title: "ประวัติออเดอร์",
-      icon: <BiTimer />,
-      Link: "/history",
+      title: 'ประวัติออเดอร์',
+      Link: '/history',
     },
     {
-      title: "เพิ่มอาหาร",
-      icon: <CgPlayListAdd />,
-      Link: "/addProduct",
+      title: 'เพิ่มอาหาร',
+      Link: '/addProduct',
     },
     {
-      title: "ลบอาหาร",
-      icon: <AiOutlineDelete />,
-      Link: "/deleteProduct",
+      title: 'ลบอาหาร',
+      Link: '/deleteProduct',
     },
     {
-      title: "ออกจากระบบ",
-      icon: <MdOutlineLogout />,
-      Link: "/login",
+      title: 'ออกจากระบบ',
+      Link: '/login',
     },
   ];
   return (
     <fragment>
       {/* <div className={`flex visible xs:invisible`}> */}
       <aside className={`h-screen w-60 bg-[#AE2C26] shadow fixed `}>
-        <div className="flex justify-center content-center align-center">
+        <div className='flex justify-center content-center align-center'>
           <img
             className={`w-4/5 cursor-pointer block float-left duration-300`}
-            src="https://cdn.discordapp.com/attachments/1038852761170739343/1043104384004919317/Posez_2.png"
-            alt=""
+            src='https://cdn.discordapp.com/attachments/1038852761170739343/1043104384004919317/Posez_2.png'
+            alt=''
           />
         </div>
-        <ul className="mb-5">
-          <li className="text-white text-sm flex items-center gap-x-4 cursor-pointer hover:bg-[#8D201A] rounded-md mt-2 p-2 border-b ">
-            <span className="text-2xl block duration-200">
-              <BsPersonCircle />
-            </span>
+        <ul className='mb-5'>
+          <li className='text-white text-sm flex items-center gap-x-4 cursor-pointer hover:bg-[#8D201A] rounded-md mt-2 p-2 border-b '>
             <span
               className={`text-base font-medium flex-1
             `}
@@ -61,17 +47,14 @@ export default function Navbar() {
             </span>
           </li>
         </ul>
-        <ul className="pt-3">
+        <ul className='pt-3'>
           {Menus.map((menu, index) => (
             <>
               <Link to={`${menu.Link}`}>
                 <li
                   key={index}
-                  className="text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[#8D201A] rounded-md mt-2 border-b"
+                  className='text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[#8D201A] rounded-md mt-2 border-b'
                 >
-                  <span className="text-lg block">
-                    {menu.icon ? menu.icon : <RiDashboardFill />}
-                  </span>
                   <span className={`text-base font-medium flex-1`}>
                     {menu.title}
                   </span>
